b49d97f021218b06c154917d1bc0b19ec8a41c48
Rafael Matos <rafaelmatosrmf01@gmail.com>
2023-08-01 20:09:53 -0300

    Update connection.php

--- sistema/connection.php
+++ sistema/connection.php
@@ -3,7 +3,7 @@
 $db_name = 'sistema';
 $db_host = 'localhost';
 $db_user = 'root';
-$db_password = '1234';
+$db_password = '';
 
 $pdo = new PDO ('mysql:dbname=' . $db_name.';host=' . $db_host, $db_user, $db_password);
 
