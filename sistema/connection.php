92de4ba1fde55267968a20d0fec322fe32a178c7
Rafael Matos <rafaelmatosrmf01@gmail.com>
2023-08-01 22:16:17 -0300

    atual

--- sistema/connection.php
+++ sistema/connection.php
@@ -6,5 +6,3 @@ $db_user = 'root';
 $db_password = '';
 
 $pdo = new PDO ('mysql:dbname=' . $db_name.';host=' . $db_host, $db_user, $db_password);
-
-?>
--- sistema/direction_estoque.php
+++ sistema/direction_estoque.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estoque</title>
     <link rel="stylesheet" href="./assets/css/style.css">
-
+    
 </head>
-
 <body>
-    <?php 
+<?php 
 
 require_once 'connection.php';
-require_once 'estoque.php';
 
 
 ?>
-    <header class="cabecalho">
+    <header class= "cabecalho">
         <div class="cabecalho_logo">
             <a href="index.php">
                 <img src="./assets/image/panflight_logo.png" width="120px" height="120px" class="logo" alt="">
@@ -27,30 +24,30 @@ require_once 'estoque.php';
             <nav>
                 <a class="navlink" href="adicionar_peça.php">Adicionar Peça</a> <br>
                 <a class="navlink" href="cadastrar_ferramenta.php">Cadastrar Ferramenta</a> <br>
-                <a class="navlink" href="direction_estoque.php">Consultar Estoque</a>
+                <a class="navlink" href="direction_estoque.php">Consultar Estoque</a> 
             </nav>
-
+              
         </div>
 
-
+   
     </header>
     <section class="consulta">
-
+    
 
         <h1>Busca por Ferramentas</h1>
         <form method="get" action="">
             <input type="text" name="busca" value="<?=!isset($_GET['busca']) ? 'PN ou Ferramenta' : $_GET['busca']?>">
             <button type="submit">Pesquisar</button>
         </form>
-
+        
         <?php 
 
+        require_once 'estoque.php';
 
 
         ?>
     </section>
 
-
+ 
 </body>
-
 </html>
